072a7af05f31f63667078b7b1aeee36d4f6978b6
Ben H <bhainesva@gmail.com>
2014-03-06 18:35:52 -0500

    Double clicking a word now defines it using aonaware services.

--- src/content/browserOverlay.js
+++ src/content/browserOverlay.js
@@ -18,10 +18,30 @@ XULSchoolChrome.BrowserOverlay = {
   //   window.alert(message);
   // }
 
-  	sayHello : function(aEvent) {
-  		var selRange = content.document.getSelection ();
-      alert(selRange.toString ());
-  	}
+    sayHello : function(aEvent) {
+      var selRange = content.document.getSelection ();
+      document.getElementById('clicktionary-definition-label').value = selRange.getRangeAt(0).toString();
+    },
+
+    define : function(aEvent) {
+      var selRange = content.document.getSelection().getRangeAt(0);
+      var word = selRange.toString();
+
+      var oReq = new XMLHttpRequest();
+      oReq.overrideMimeType("text/xml");
+      oReq.onload = function reqListener () {
+        var fullDef = this.responseXML.firstChild.textContent;
+        var defOne = fullDef.substr(fullDef.indexOf("1."), fullDef.substr(fullDef.indexOf("1.")).indexOf(";"));
+        var defOnePretty = (defOne.substr(3) + ".").replace(/\s+/g, " ");
+        console.log(defOnePretty);
+        document.getElementById('clicktionary-definition-label').value = defOnePretty;        
+      };
+
+      oReq.open("get", "http://services.aonaware.com//DictService/DictService.asmx/DefineInDict?dictId=gcide&word=" + word, true);
+      oReq.send();
+
+
+    }
 };
 
 
