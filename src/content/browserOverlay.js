a2f00e92322034ca2c4d2a8987ca60e5f48f8047
Ben H <bhainesva@gmail.com>
2014-03-07 12:00:40 -0500

    Updates old xulschool namespace.

--- src/content/browserOverlay.js
+++ src/content/browserOverlay.js
@@ -1,22 +1,14 @@
 /**
- * XULSchoolChrome namespace.
+ * Clicktionary namespace.
  */
 
-var XULSchoolChrome = {};
+var Clicktionary = {};
 
 /**
  * Controls the browser overlay for the Hello World extension.
  */
-XULSchoolChrome.BrowserOverlay = {
-  /**
-   * Says 'Hello' to the user.
-   */
-  // sayHello : function(aEvent) {
-  //   let stringBundle = document.getElementById("xulschoolhello-string-bundle");
-  //   let message = stringBundle.getString("xulschoolhello.greeting.label");
+Clicktionary.BrowserOverlay = {
 
-  //   window.alert(message);
-  // }
 
     sayHello : function(aEvent) {
       var selRange = content.document.getSelection ();
