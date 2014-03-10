16b663b6e0bc91723d4ebce00ba2636f4dd5b5c8
Ben H <bhainesva@gmail.com>
2014-03-10 16:30:40 -0400

    Displays message for undefined words. Implements very primitive parsing for alternate format definitions, ex: 'chrome'

--- src/content/browserOverlay.js
+++ src/content/browserOverlay.js
@@ -30,17 +30,44 @@ Clicktionary.BrowserOverlay = {
       var oReq = new XMLHttpRequest();
       oReq.overrideMimeType("text/xml");
       oReq.onload = function reqListener () {
-        var fullDef = this.responseXML.firstChild.textContent;
+        console.log(this.responseXML.firstChild.textContent);
+        var fullDef = Clicktionary.BrowserOverlay.parseDefinition(this.responseXML.firstChild.textContent);
         var defOne = fullDef.substr(fullDef.indexOf("1."), fullDef.substr(fullDef.indexOf("1.")).indexOf(";"));
         var defOnePretty = (defOne.substr(3) + ".").replace(/\s+/g, " ");
         console.log(defOnePretty);
-        document.getElementById('clicktionary-definition-label').value = defOnePretty;        
+        document.getElementById('clicktionary-definition-label').value = fullDef;        
       };
 
       oReq.open("get", "http://services.aonaware.com//DictService/DictService.asmx/DefineInDict?dictId=gcide&word=" + word, true);
       oReq.send();
+    },
 
+    parseDefinition : function(definition) {
+      if (definition.indexOf("1. ") != -1) {
+        var defOne = definition.substr(definition.indexOf("1."), definition.substr(definition.indexOf("1.")).indexOf(";"));
+        console.log(defOne);
+        var defOnePretty = (defOne.substr(3) + ".").replace(/\s+/g, " ");
+      }
+      else {
+        var defOne = definition.substr(definition.indexOf("."), definition.substr(definition.indexOf(".")).indexOf("."));
+        arrayOfLines = definition.match(/[^\r\n]+/g);
+        for(var i = arrayOfLines.length - 1; i >= 0; i--) {
+          arrayOfLines[i] = arrayOfLines[i].trim();
+          if(!arrayOfLines[i] || /^\s*$/.test(arrayOfLines[i])) {
+             arrayOfLines.splice(i, 1);
+          }
+        }
+        if(arrayOfLines.length == 1) {
+          return "No definition found."
+        }
 
+        console.log(arrayOfLines);
+        console.log(defOne);
+        var defOnePretty = arrayOfLines[5];
+      }
+
+      console.log(defOnePretty);
+      return defOnePretty;
     }
 };
 
